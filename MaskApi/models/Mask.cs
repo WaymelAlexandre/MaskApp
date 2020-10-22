984dbd865b555ccb61c24af63ca26b692e2ee22d
alex w <103083020@student.swin.edu.au>
2020-10-22 16:22:10 +1100

    remove the test doing just the Api exercise

--- MaskApi/models/Mask.cs
+++ MaskApi/models/Mask.cs
@@ -2,6 +2,30 @@ namespace MaskApi.models
 {
     public class Mask
     {
-        
+        public string MaskId { get; set; }
+        public int Size { get; set; }
+        public string  Colour { get; set; }
+        public bool New { get; set; }
+        public bool PreInfected { get; set; }
+        public string Design { get; set; }
+        public bool Reusable { get; set; }
+        public double Cost { get; set; }
+    
+    
+        public Mask(string maskId, int size, string colour, bool @new, bool preInfected, string design, bool reusable, double cost)
+        {
+            this.MaskId = maskId;
+            this.Size = size;
+            this.Colour = colour;
+            this.New = @new;
+            this.PreInfected = preInfected;
+            this.Design = design;
+            this.Reusable = reusable;
+            this.Cost = cost;
+        }
+            public Mask(){ }
+
+    
+
     }
 }
--- MaskApi/models/Order.cs
+++ MaskApi/models/Order.cs
@@ -1,7 +1,33 @@
+using System;
+
 namespace MaskApi.models
 {
-    public class Order
-    {
+    public class Order{
         
+        public int OrderNo { get; set; }
+        public Mask Mask { get; set; }
+        public int Qty { get; set; }
+        public double Price { get; set; }
+
+        public Order(int orderNo, Mask mask, int qty, double price)
+        {
+            this.OrderNo = this.GenOrderNo();
+            this.Mask = mask;
+            this.Qty = qty;
+            this.setPrice();
+        }
+        public Order(){}
+
+        public void setPrice()
+        {
+            this.Price = this.Qty * Mask.Cost;
+            
+        }
+
+        public int GenOrderNo(){
+            Random rand =new Random();
+            return rand.Next
+            (1000,10000);
+        }
     }
 }
